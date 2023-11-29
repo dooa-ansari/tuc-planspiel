0156b61535a5943ad0b425a016dd3854e97345c3
dooamindshine <dooa@mindshine.app>
2023-11-29 19:54:06 +0100

    department name query parameter fixed

--- Backend/across/polls/views.py
+++ Backend/across/polls/views.py
@@ -5,17 +5,18 @@ graph.parse("web_engineering_modules.rdf")
 graph.parse("departments.rdf")
 
 module_list = """
-SELECT DISTINCT ?moduleName ?moduleId ?deptName
+SELECT ?moduleName ?moduleId ?deptName ?dName
 WHERE {
-    ?name <http://tuc.web.engineering/module#hasName> ?moduleName .
-    ?id   <http://tuc.web.engineering/module#hasModuleNumber> ?moduleId .
-    ?deptartment <http://tuc.web.engineering/department#hasName> ?deptName .
+    ?name <http://tuc.web.engineering/module#hasName> ?moduleName ;
+          <http://tuc.web.engineering/module#hasModuleNumber> ?moduleId ;
+          <http://tuc.web.engineering/department#hasName> ?deptName .
+    ?deptName <http://tuc.web.engineering/department#hasName> ?dName .
 }"""
 
 qresponse = graph.query(module_list)
 data = "<html><body>"
 for row in qresponse:
-    data = data + f"<p><b>{row.moduleName}</b> has id <i>{row.moduleId}</i> and belongs to department <u>{row.deptName}</u></p>"
+    data = data + f"<p><b>{row.moduleName}</b> has id <i><b>{row.moduleId}</b></i> and belongs to department <u><b>{row.dName}</b></u></p>"
 
 data = data + "</html></body>"
 
