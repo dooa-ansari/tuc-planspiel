4f8824c664e06f75311b6ae9f2065a165da91bbe
akshayghavale <akshayghavale1998@gmail.com>
2023-11-30 20:17:05 +0100

    Returning module data in JSON format and changed departments and module rdf

--- Backend/across/polls/views.py
+++ Backend/across/polls/views.py
@@ -1,24 +1,43 @@
+import json
+
 import rdflib
 from django.http import HttpResponse
-graph = rdflib.Graph()
-graph.parse("web_engineering_modules.rdf")
-graph.parse("departments.rdf")
 
+graph = rdflib.Graph()
+graph.parse("D:\Web Engineering\SEM-III\Planspiel\ACROSS\ACROSS_MAIN\web-wizards\Backend\web_engineering_modules.rdf")
+graph.parse("D:\Web Engineering\SEM-III\Planspiel\ACROSS\ACROSS_MAIN\web-wizards\Backend\departments.rdf")
 module_list = """
-SELECT ?moduleName ?moduleId ?deptName ?dName
+SELECT ?moduleName ?moduleId ?moduleContent ?moduleCreditPoints ?deptName ?dName ?deptId
 WHERE {
     ?name <http://tuc.web.engineering/module#hasName> ?moduleName ;
           <http://tuc.web.engineering/module#hasModuleNumber> ?moduleId ;
+          <http://tuc.web.engineering/module#hasContent> ?moduleContent ;
+          <http://tuc.web.engineering/module#hasCreditPoints> ?moduleCreditPoints ; 
           <http://tuc.web.engineering/department#hasName> ?deptName .
     ?deptName <http://tuc.web.engineering/department#hasName> ?dName .
-}"""
+    ?deptName <http://tuc.web.engineering/department#hasDeptId> ?deptId .
+}
+"""
 
 qresponse = graph.query(module_list)
 data = "<html><body>"
+counter = 0
+data_list = []
 for row in qresponse:
-    data = data + f"<p><b>{row.moduleName}</b> has id <i><b>{row.moduleId}</b></i> and belongs to department <u><b>{row.dName}</b></u></p>"
+    counter = counter + 1
+    data_dict = {
+        'moduleName': str(row.moduleName),
+        'moduleId': str(row.moduleId),
+        'moduleContent': str(row.moduleContent),
+        'moduleCreditPoints': str(row.moduleCreditPoints),
+        'deptName': str(row.dName),
+        'deptId': str(row.deptId)
+    }
+    data_list.append(data_dict)
+json_data = json.dumps(data_list, indent=2)
+data = data + json_data
+data = data + f"<p>Total Modules are: {counter} </p></html></body>"
 
-data = data + "</html></body>"
 
 def index(request):
-    return HttpResponse(data)
+    return HttpResponse(json_data)
