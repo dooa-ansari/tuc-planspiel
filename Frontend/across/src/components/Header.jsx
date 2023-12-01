19b22fe7f5c7e2c80b131aa9840ddde8d9bf6310
SAROJ SAH <sarojsaroj390@gmail.com>
2023-12-01 10:31:15 +0100

    modified header section

--- Frontend/across/src/components/Header.jsx
+++ Frontend/across/src/components/Header.jsx
@@ -1,30 +1,57 @@
 import React from "react";
 import "../assets/css/Header.css";
 import { Link } from "react-scroll";
+import brandLogo from "../assets/brandLogo.png";
 
 const duration = 500;
 const Header = ({ brandName }) => {
   return (
     <div className="header">
       <div className="header__contents">
-        <h2 className="header__brandName">{brandName}</h2>
-        <ul className="header__links">
+        <div className="header__left">
+          <img src={brandLogo} className="header__brandImage" />
+        </div>
+        <div className="header__middle">
+          <ul className="header__links">
+            <li className="header__link">
+              <Link activeClass="active" to="#" spy smooth duration={duration}>
+                Home
+              </Link>
+            </li>
+            <li className="header__link">
+              <Link activeClass="active" to="#" spy smooth duration={duration}>
+                About
+              </Link>
+            </li>
+            <li className="header__link">
+              <Link activeClass="active" to="#" spy smooth duration={duration}>
+                Universities
+              </Link>
+            </li>
+            <li className="header__link">
+              <Link activeClass="active" to="#" spy smooth duration={duration}>
+                ACROSS
+              </Link>
+            </li>
+            <li className="header__link">
+              <Link activeClass="active" to="#" spy smooth duration={duration}>
+                Reach Us
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="header__right">
           <li className="header__link">
             <Link activeClass="active" to="#" spy smooth duration={duration}>
-              Home
-            </Link>
-          </li>
-          <li className="header__link">
-            <Link activeClass="active" to="#" spy smooth duration={duration}>
-              About
+              Log in
             </Link>
           </li>
           <li className="header__link header__signup-btn">
             <Link to="#" spy smooth duration={duration}>
-              Create Account
+              Sign Up
             </Link>
           </li>
-        </ul>
+        </div>
       </div>
     </div>
   );
