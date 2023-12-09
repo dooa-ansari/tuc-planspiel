0f9e0dd56c27d1074956fca1fd01d8c9e0fe778d
dooamindshine <dooa@mindshine.app>
2023-12-09 22:56:44 +0100

    pons translator word by word and full text translation work in progress for POC

--- Backend/across/polls/translator.py
+++ Backend/across/polls/translator.py
@@ -1,5 +1,7 @@
 from .read_rdf_file import readRDFFile
-from deep_translator import GoogleTranslator
+from deep_translator import GoogleTranslator, PonsTranslator
+from ponstrans import translate
+
 import json
 
 def translateText():
@@ -7,11 +9,30 @@ def translateText():
    data_list = []
    for row in web_engineering_modules:
       translated = GoogleTranslator(source='de', target='en').translate(row.moduleContent)
+      translatedWordList = translate(row.moduleContent, source_language="de", target_language="en")
+      # try:
+      # #  translatedWordList = PonsTranslator(source='de', target='en').translate('Medienretrieval', return_all=False)
+      #  translatedWordList = translate(word="Medienretrieval", source_language="de", target_language="en")
+      # except Exception as e:
+      #   print(e) 
+          
+      # words = row.moduleContent.split(" ")
+      # translatedWordList = ""
+      # try:
+      #  for word in words:
+          
+         
+      # except Exception as e:
+      #   print(e)   
+      
       data_dict = {
         'name': str(row.moduleName),
         'moduleContent': str(translated),
+        'ponTranslation': str(''.join(translatedWordList))
       }
       data_list.append(data_dict)
    json_data = json.dumps(data_list, indent=2)
 
    return json_data
+
+# add word translation caching in order to avoid too many api calls
