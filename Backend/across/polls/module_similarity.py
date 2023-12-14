c9b4b9890452c69d3921152357e0e6e4ad2edfd6
dooamindshine <dooa@mindshine.app>
2023-12-14 20:53:20 +0100

    extra imports removed

--- Backend/across/polls/module_similarity.py
+++ Backend/across/polls/module_similarity.py
@@ -1,21 +1,6 @@
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 from .translator import translateModules
 from .update_rdf_module import add_predicate_for_module_similarity
-import json
 import ssl
-import numpy as np
-import sklearn
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from transformers import pipeline
-import fasttext
-import fasttext.util
-import torch
 import spacy
 
 def read_modules_and_compare(universityOneModulesFile, univeristyTwoModulesFile):
@@ -32,7 +17,6 @@ def read_modules_and_compare(universityOneModulesFile, univeristyTwoModulesFile)
     data_list = []
     for module in firstUniversityModules:
         for module2 in secondUniversityModules:
-            # similarity = find_text_similarity_pytorch(module.moduleContent, module2.moduleContent)
             similarity = find_text_similarity_spacy(module.moduleContent, module2.moduleContent)
             print(f"{module.name} - {module2.name} - {module.uri} similarity value is : {similarity}")
             if(similarity):
@@ -43,126 +27,10 @@ def read_modules_and_compare(universityOneModulesFile, univeristyTwoModulesFile)
                module2['similar_modules'] = similar_modules
                data_list.append(module)
                data_list.append(module2)
-            # data_dict = {
-            # 'name': str(module.moduleName),
-            # 'similarity': str(similarity),
-            # }
-            # data_list.append(data_dict)
+           
     add_predicate_for_module_similarity(universityOneModulesFile, univeristyTwoModulesFile, data_list)
     return {}
     
-def find_text_similarity(module1Content, module2Content):
-    # Tokenize and lemmatize the texts
-    similarity = 0
-    try:
-     tokens1 = word_tokenize(module1Content)
-     tokens2 = word_tokenize(module2Content)
-     lemmatizer = WordNetLemmatizer()
-     tokens1 = [lemmatizer.lemmatize(token) for token in tokens1]
-     tokens2 = [lemmatizer.lemmatize(token) for token in tokens2]
-
-     # Remove stopwords
-     stop_words = stopwords.words('english')
-     tokens1 = [token for token in tokens1 if token not in stop_words]
-     tokens2 = [token for token in tokens2 if token not in stop_words]
-
-     # Create the TF-IDF vectors
-     vectorizer = TfidfVectorizer()
-     vector1 = vectorizer.fit_transform(tokens1)
-     vector2 = vectorizer.transform(tokens2)
-
-     # Calculate the cosine similarity
-     similarity = cosine_similarity(vector1, vector2)
-    except Exception as e:
-       print("error"+e) 
-    # 
-
-    return similarity
-
-
-def find_text_similarity_sk(module1Content, module2Content):
-    # Tokenize and lemmatize the texts
-    similarity = 0
-    try:
-     vectorizer = TfidfVectorizer()
-     vectors = vectorizer.fit_transform([module1Content, module2Content])
-
-     # Calculate the cosine similarity between the vectors
-     similarity = cosine_similarity(vectors)
-
-    except Exception as e:
-       print("error"+e) 
-    # 
-
-    return similarity
-
-def find_text_similarity_bert(module1Content, module2Content):
-    # Tokenize and lemmatize the texts
-    similarity = 0
-    try:
-     # Load the BERT model
-     model = pipeline("fill-mask", model="bert-base-uncased")
-
-     # Tokenize and encode the texts
-     text1 = "This is the first text."
-     text2 = "This is the second text."
-     encoding1 = model.encode(text1, max_length=512)
-     encoding2 = model.encode(text2, max_length=512)
-
-     # Calculate the cosine similarity between the embeddings
-     similarity = numpy.dot(encoding1, encoding2) / (numpy.linalg.norm(encoding1) * numpy.linalg.norm(encoding2))
-    #  print(similarity)
-
-    except Exception as e:
-       print("error"+e) 
-    # 
-
-    return similarity
-
-
-def find_text_similarity_fast_text(module1Content, module2Content):
-    # Load the FastText model
-    fasttext.util.download_model('en', if_exists='ignore')  # English
-    model = fasttext.load_model('cc.en.300.bin')
-
-    # Preprocess the text
-    text1 = 'This is a piece of text'
-    text2 = 'This is another piece of text'
-    tokens1 = fasttext.tokenize(text1)
-    tokens2 = fasttext.tokenize(text2)
-    tokens1 = [token.lower() for token in tokens1]
-    tokens2 = [token.lower() for token in tokens2]
-
-    # Generate word vectors for each piece of text
-    try:
-     vector1 = model.get_sentence_vector(tokens1)
-     vector2 = model.get_sentence_vector(tokens2)
-    except Exception as e:
-      print("error:"+e) 
-
-    # # Calculate the similarity between the vectors using cosine similarity
-    # from scipy.spatial.distance import cosine
-    # similarity = 1 - cosine(vector1, vector2)
-    # print(similarity)
-    print('Similarity:', 0)
-
-def find_text_similarity_pytorch(text1, text2):
-    # Convert the texts to tensors
-    text1 = torch.tensor([text1])
-    text2 = torch.tensor([text2])
-
-    # Calculate the dot product of the texts
-    dot_product = torch.matmul(text1, text2.transpose(1, 0))
-
-    # Calculate the norms of the texts
-    norm1 = torch.norm(text1, dim=1)
-    norm2 = torch.norm(text2, dim=1)
-
-    # Calculate the cosine similarity
-    cosine_similarity = dot_product / (norm1 * norm2)
-    print("similarity:"+cosine_similarity)
-    return cosine_similarity
-
 
 def find_text_similarity_spacy(module1Content, module2Content):
     nlp = spacy.load('en_core_web_lg')
@@ -185,6 +53,3 @@ def find_text_similarity_spacy(module1Content, module2Content):
     
     is_similar = True if(verbs_similarity > 0.2 and adj_similarity > 0.2 and noun_similarity > 0.5) else False
     return is_similar
-    # print(f"{s1} and {s2} VERBS: {nlp(s1_verbs).similarity(nlp(s2_verbs))}")
-    # print(f"{s1} and {s2} ADJ: {nlp(s1_adjs).similarity(nlp(s2_adjs))}")
-    # print(f"{s1} and {s2} NOUNS: {nlp(s1_nouns).similarity(nlp(s2_nouns))}")
