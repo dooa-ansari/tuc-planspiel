44bea92474cbb7abce4d1aa8ab57e1bf54558a45
dooa.ansari <dooa.ansari@s2022.tu-chemnitz.de>
2024-01-06 14:52:53 +0100

    token flow in prgress

--- Frontend/across/src/admin/Home.jsx
+++ Frontend/across/src/admin/Home.jsx
@@ -1,10 +1,18 @@
 // src/admin/Home.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [token, setToken] = useState(null)
+
+    useEffect(() => {
+        const authToken = localStorage.getItem('authToken');
+        setToken(authToken)
+    }, [])
+    console.log(token)
     return (
         <div>
             <h2>Welcome to the Admin Panel!</h2>
+            {token}
             {/* Add more content as needed */}
         </div>
     );
