e2ac212c70e206f321cb4527fd899fa8cb568ae0
Akshay Ghavale <akshay.ghavale@s2022.tu-chemnitz.de>
2024-01-06 11:12:36 +0100

    API created for delete individual module

--- Backend/across/adminapp/urls.py
+++ Backend/across/adminapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import upload_file, update_module
+from .views import upload_file, update_module, delete_module
 
 
 urlpatterns= [
     path('api/upload/', upload_file, name='upload_file'),
     path('api/insert/', update_module, name='update_module'),
+    path('api/delete/', delete_module, name='delete_module'),
 ]
