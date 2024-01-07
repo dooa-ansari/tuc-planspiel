416a8bbf713054489c8a91a23d49aa77c59fbb05
Akshay Ghavale <akshay.ghavale@s2022.tu-chemnitz.de>
2024-01-07 20:14:02 +0100

    get list of all universities and API for select university

--- Backend/across/polls/sparql.py
+++ Backend/across/polls/sparql.py
@@ -201,3 +201,14 @@ def delete_individual_module(module_uri):
     """
 
     return query  
+
+def get_university_list():
+    query = """
+    SELECT ?universityName 
+        WHERE {
+            ?university rdf:type <http://across/university#> .
+            ?university <http://across/university#hasUniversityName> ?universityName .        
+        }
+    """
+
+    return query
