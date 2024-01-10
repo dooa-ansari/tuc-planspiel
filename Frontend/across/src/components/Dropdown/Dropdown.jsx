74c22d0fa4ac3760c6fcdbb1b404c5d3682c25d5
Saroj Sah <saroj.sah@s2022.tu-chemnitz.de>
2024-01-11 00:57:00 +0100

    built compare modules page from scratch

--- Frontend/across/src/components/Dropdown/Dropdown.jsx
+++ Frontend/across/src/components/Dropdown/Dropdown.jsx
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
-import { FaAngleDown } from "react-icons/fa6";
+import React, { useEffect, useState, useRef } from "react";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa6";
+
 import "./Dropdown.css";
 
 const Dropdown = ({ options, value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const divElement = useRef();
+
+  useEffect(() => {
+    const handler = event => {
+      if (!divElement.current) {
+        return;
+      }
+      if (!divElement.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handler, true);
+
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
 
   const handleClick = () => {
     setIsOpen(currentIsOpen => !currentIsOpen);
@@ -26,11 +45,21 @@ const Dropdown = ({ options, value, onChange }) => {
     );
   });
 
+  const zIndexValue = isOpen ? 1000 : 1;
+
   return (
-    <div className="dropdownWrapper">
+    <div
+      className="dropdownWrapper"
+      ref={divElement}
+      style={{ zIndex: zIndexValue }}
+    >
       <div className="dropdown__options" onClick={handleClick}>
         {value?.label || "Select ..."}
-        <FaAngleDown style={{ fontSize: "1.125rem", lineHeight: "1.75rem" }} />
+        {isOpen ? (
+          <FaAngleUp className="angleIcon" />
+        ) : (
+          <FaAngleDown className="angleIcon" />
+        )}
       </div>
       {isOpen && (
         <div className="dropdown__open-options">{renderedOptions}</div>
--- Frontend/across/src/pages/user/CompareModules/CompareModules.jsx
+++ Frontend/across/src/pages/user/CompareModules/CompareModules.jsx
@@ -3,33 +3,110 @@ import "./CompareModules.css";
 import MainLayout from "../../../components/user/MainLayout/MainLayout";
 
 import Dropdown from "../../../components/Dropdown/Dropdown";
+import SearchBox from "../../../components/user/SearchBox/SearchBox";
 
 const CompareModules = () => {
-  const [selection, setSelection] = useState(null);
+  const [selectedUniversity, setSelectedUniversity] = useState(null);
+  const [selectedModule, setSelectedModule] = useState(null);
+  const [selectedCourse, setSelectedCourse] = useState(null);
 
-  const handleChange = option => {
-    setSelection(option);
+  const handleUniversityChange = university => {
+    setSelectedUniversity(university);
+  };
+  const handleModuleChange = module => {
+    setSelectedModule(module);
+  };
+  const handleCourseChange = course => {
+    setSelectedCourse(course);
   };
 
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" },
+  const universities = [
+    {
+      label: "Bialystok University of Technology",
+      value: "Bialystok University of Technology",
+    },
+    {
+      label: "Chemnitz University of Technology",
+      value: "Chemnitz University of Technology",
+    },
+    { label: "University of Craiova", value: "University of Craiova" },
+    { label: "University of Girona", value: "University of Girona" },
+    { label: "University of Lleida", value: "University of Lleida" },
+  ];
+
+  const modules = [
+    { label: "Module1", value: "module1" },
+    { label: "Module2", value: "module2" },
+  ];
+
+  const courses = [
+    { label: "Course1", value: "course1" },
+    { label: "Course2", value: "course2" },
   ];
   return (
     <>
       <MainLayout>
-        <h1>Compare Modules Page</h1>
-        <div style={{ display: "flex" }}>
+        <SearchBox />
+        <h1 style={{ textAlign: "center" }}>Compare Modules Page</h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            marginBottom: "40px",
+            paddingTop: "70px",
+          }}
+        >
+          <h4 style={{ width: "10%" }}>Universities</h4>
           <Dropdown
-            options={options}
-            value={selection}
-            onChange={handleChange}
+            options={universities}
+            value={selectedUniversity}
+            onChange={handleUniversityChange}
           />
           <Dropdown
-            options={options}
-            value={selection}
-            onChange={handleChange}
+            options={universities}
+            value={selectedUniversity}
+            onChange={handleUniversityChange}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            marginBottom: "40px",
+          }}
+        >
+          <h4 style={{ width: "10%" }}>Modules</h4>
+          <Dropdown
+            options={modules}
+            value={selectedModule}
+            onChange={handleModuleChange}
+          />
+          <Dropdown
+            options={modules}
+            value={selectedModule}
+            onChange={handleModuleChange}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            marginBottom: "40px",
+          }}
+        >
+          <h4 style={{ width: "10%" }}>Courses</h4>
+          <Dropdown
+            options={courses}
+            value={selectedCourse}
+            onChange={handleCourseChange}
+          />
+          <Dropdown
+            options={courses}
+            value={selectedCourse}
+            onChange={handleCourseChange}
           />
         </div>
       </MainLayout>
