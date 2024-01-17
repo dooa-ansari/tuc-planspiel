d2f58872adc3b38f26540aaceeffa91023679b00
Saroj Sah <saroj.sah@s2022.tu-chemnitz.de>
2024-01-17 22:03:16 +0100

    added responsiveness to landing page

--- Frontend/across/src/components/Header.jsx
+++ Frontend/across/src/components/Header.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/css/Header.css";
 import { Link } from "react-scroll";
 import { NavLink } from "react-router-dom";
 import brandLogo from "../assets/images/brandLogo.png";
+import MenuCloseButton from "./MenuCloseButton/MenuCloseButton";
+import MenuOpenButton from "./MenuOpenButton/MenuOpenButton";
 
 const duration = 500;
-const Header = ({ brandName }) => {
+const Header = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenuStatus = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="header">
       <div className="header__contents">
@@ -14,10 +22,11 @@ const Header = ({ brandName }) => {
             <img src={brandLogo} className="header__brandImage" />
           </NavLink>
         </div>
+
         <div className="header__middle">
           <ul className="header__links">
             <li className="header__link">
-              <Link activeClass="active" to="#" spy smooth duration={duration}>
+              <Link activeClass="active" to="/" spy smooth duration={duration}>
                 Home
               </Link>
             </li>
@@ -49,6 +58,7 @@ const Header = ({ brandName }) => {
             </li>
           </ul>
         </div>
+
         <div className="header__right">
           <li className="header__link">
             <NavLink className="login__link" to="/campus-flow/login">
@@ -61,6 +71,22 @@ const Header = ({ brandName }) => {
             </NavLink>
           </li>
         </div>
+
+        {isMenuOpen ? (
+          <Link
+            className="header__menu-icons header__menu-open-icon"
+            onClick={toggleMenuStatus}
+          >
+            <MenuCloseButton />
+          </Link>
+        ) : (
+          <Link
+            className="header__menu-icons header__menu-open-icon"
+            onClick={toggleMenuStatus}
+          >
+            <MenuOpenButton />
+          </Link>
+        )}
       </div>
     </div>
   );
--- Frontend/across/src/components/user/Navbar/Navbar.jsx
+++ Frontend/across/src/components/user/Navbar/Navbar.jsx
@@ -8,6 +8,7 @@ import { RiLogoutBoxRFill } from "react-icons/ri";
 import enLanSvg from "../../../assets/images/en.svg";
 
 import { useAuth } from "../../../context/AuthContext";
+import MenuOpenButton from "../../MenuOpenButton/MenuOpenButton";
 
 const Navbar = () => {
   const [auth, setAuth] = useAuth();
@@ -52,46 +53,10 @@ const Navbar = () => {
                 <Link className="navbar__link">
                   <img src={enLanSvg} alt="great britain flag" />
                 </Link>
-                <button id="menu-button">
-                  <svg width="40" height="40" viewBox="0 0 40 40" fill="none">
-                    <rect
-                      width="40"
-                      height="40"
-                      rx="20"
-                      fill="black"
-                      fillOpacity="0.1"
-                    />
-                    <path
-                      d="M12.9719 16H25C25.5531 16 26 15.5531 26 15C26 14.4469 25.5531 14 25 14H12.9719C12.4478 14 12 14.4478 12 14.9719C12 15.4959 12.4478 16 12.9719 16ZM27 19H14.9719C14.4478 19 14 19.4469 14 20C14 20.5531 14.4478 21 14.9719 21H26.9719C27.5531 21 28 20.5531 28 20C28 19.4469 27.5531 19 27 19ZM25 24H12.9719C12.4478 24 12 24.4469 12 24.9719C12 25.4969 12.4478 26 12.9719 26H25C25.5531 26 26 25.5531 26 25C26 24.4469 25.5531 24 25 24Z"
-                      fill="black"
-                      fillOpacity="0.6"
-                    />
-                  </svg>
-                </button>
-                <button id="menu-close-button">
-                  <svg width="40" height="40" viewBox="0 0 40 40" fill="none">
-                    <rect
-                      width="40"
-                      height="40"
-                      rx="20"
-                      fill="black"
-                      fillOpacity="0.1"
-                    />
 
-                    <path
-                      d="M12.9719 16H30C30.5531 16 31 15.5531 31 15C31 14.4469 30.5531 14 30 14H12.9719C12.4478 14 12 14.4478 12 14.9719C12 15.4959 12.4478 16 12.9719 16Z"
-                      fill="black"
-                      fillOpacity="0.6"
-                      transform="translate(-6, 25) rotate(-45)"
-                    />
-                    <path
-                      d="M12.9719 26H30C30.5531 26 31 25.5531 31 25C31 24.4469 30.5531 24 30 24H12.9719C12.4478 24 12 24.4478 12 24.9719C12 25.4959 12.4478 26 12.9719 26Z"
-                      fill="black"
-                      fillOpacity="0.6"
-                      transform="translate(21, -12) rotate(45)"
-                    />
-                  </svg>
-                </button>
+                <Link className="menu-open-button">
+                  <MenuOpenButton />
+                </Link>
               </ul>
             </nav>
           </header>
