30912a0ddd67859ceb997aa7cf528e7d44b46f2a
Saroj Sah <saroj.sah@s2022.tu-chemnitz.de>
2024-01-20 19:37:04 +0100

    updated pnf contents

--- Frontend/across/src/pages/PageNotFound.jsx
+++ Frontend/across/src/pages/PageNotFound.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../assets/css/PageNotFound.css";
+import pnfImage from "../assets/images/pnf-image.svg";
 
 const PageNotFound = () => {
   return (
     <div className="pnf">
+      <img className="pnf__image" src={pnfImage} alt="" />
       <h1 className="pnf__title">Page Not Found</h1>
-      <p>Oops! Something went wrong..</p>
+      <p>Something went wrong..</p>
       <button type="button" className="pnf__button">
         Go back
       </button>
