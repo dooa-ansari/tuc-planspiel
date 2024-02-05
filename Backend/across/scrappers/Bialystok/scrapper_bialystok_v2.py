dd68ce37ec6b4d31f930e648f2e7a5c0ba1c5df2
dooa.ansari <dooa.ansari@s2022.tu-chemnitz.de>
2024-02-05 12:55:22 +0100

    merged pdf also read

--- Backend/across/scrappers/Bialystok/scrapper_bialystok_v2.py
+++ Backend/across/scrappers/Bialystok/scrapper_bialystok_v2.py
@@ -52,6 +52,7 @@ else:
      ssl._create_default_https_context = _create_unverified_https_context
     
 
+
 # Download all pdfs of modules only once to be called
      
 # html = urllib.request.urlopen("https://pb.edu.pl/erasmus-course-catalogue/?sem&f=faculty-of-computer-science&ects")
@@ -68,6 +69,8 @@ else:
 #      wget.download(pdf_url)
 #    break 
     #  print(pdf.read())
+
+
 
 field_of_study_pattern = re.compile(r'(?<=Faculty of )(.*?)(?= Field of study)')
 field_of_study_pattern_v2 = re.compile(r'(?<=Field of study )(.*?)(?= Degree level)')
@@ -213,77 +216,4 @@ bialystok_modules_file.close()
 
 print(departments)
 
-# module_name_pattern = re.compile(r"Degree level and programme type\s*(.+)")
-# pattern = re.compile(r'Degree level and programme type(.*?)Specialization/diploma path', re.DOTALL)
-# found_text = pattern.search(text)
-# print(found_text)
-# if found_text:
-#         print(found_text)
-
-# print(text)
-
-# page = pdf_reader.getPage(0)
-    #  print(page.extractText())  
-   
-  
-#    if(all_list_a.length > 0):
-#     print(all_list_a[0])
-   
-   
-# for page in pages:
-#     html = urllib.request.urlopen(f"{base_url}en/courses/list?page={page}")
-#     data = html.read()
-#     parser = BeautifulSoup(data, 'html.parser')
-#     all_list_a = parser.find_all("a")
-#     module_id_list = []
-#     for course in all_list_a:
-#      print("connecting: "+str(page))
-#      url = course.get("href")
-#      if('/en/courses/view?prz_kod=' in url):
-#         module_id_list.append(url)
-
-#     count = 0
-#     for id_url in module_id_list:
-#      course_data_html = urllib.request.urlopen(f"{base_url}{id_url}")
-#      data_courses = course_data_html.read()
-#      parser_courses = BeautifulSoup(data_courses, 'html.parser')
-#      module_name = parser_courses.find("h1").getText().split("\n")
-#      module_name_value = module_name[1].lstrip(' ')
-#      module_id =  id_url.split("=")[1]
-#      module_content = parser_courses.find("div", class_="opis iml").getText()
-#      credit_points = parser_courses.find("div", class_="item punkty_ects")
-#      department = parser_courses.find("div", class_="item jednostka").find("a").getText()
-#      credit_points_value = credit_points.getText()
-#      credit_points_value_number = re.findall("\d+", credit_points_value)
-#      credit_points_value_to_be_added = credit_points_value_number[0]  if(len(credit_points_value_number)) > 2 else 6
-     
-#      uri_end = ''.join(e for e in module_id if e.isalnum())
-#      print(uri_end)
-#      if(uri_end!="None" and uri_end!="inPolish" and department == DEPARTMENT_CIVIL_ENGINEERING):
-#         print(f"{base_url}{id_url}")
-#         module_uri_g = URIRef(f"{uri_main}{uri_end}")
-#         uriUniversity = URIRef("http://across/university#BU")
-#         uriCourse = URIRef("http://tuc/course#Civil_Engineering_and_Environmental_Sciences")
-#         module_name_g = Literal(module_name_value, datatype=XSD.string)
-#         module_content_g = Literal(module_content, datatype=XSD.string)
-#         module_id_g = Literal(module_id, datatype=XSD.string)
-#         credit_points_g = Literal(credit_points_value_to_be_added, datatype=XSD.string)
-#         department_g = Literal(department, datatype=XSD.string)
-#         university_g = Literal("", datatype=XSD.string)
-        
-#         if module_name_g not in added_module_names and not is_module_name_in_graph(graph, module_name_g):
-#             graph.add((module_uri_g, RDF.type, NAME_SPACE.module))
-#             graph.add((module_uri_g, URIRef("http://tuc.web.engineering/module#hasName"), module_name_g))
-#             graph.add((module_uri_g, URIRef("http://tuc.web.engineering/module#hasModuleNumber"), module_id_g))
-#             graph.add((module_uri_g, URIRef("http://tuc.web.engineering/module#hasContent"), module_content_g))
-#             graph.add((module_uri_g, URIRef("http://tuc.web.engineering/module#hasCreditPoints"), credit_points_g))
-#             graph.add((module_uri_g, URIRef("http://tuc/course#hasCourse"), uriCourse))
-#             graph.add((module_uri_g, URIRef("http://across/university#hasUniversity"), uriUniversity))
-        
-# bialystok_modules_data = graph.serialize(format='xml')
-# with open('bu_Civil_Engineering_and_Environmental_Sciences.rdf', 'w', encoding='utf-8') as bialystok_modules_file:
-#     bialystok_modules_file.write(bialystok_modules_data)
-# bialystok_modules_file.close()
-    
-# html.close()
 
