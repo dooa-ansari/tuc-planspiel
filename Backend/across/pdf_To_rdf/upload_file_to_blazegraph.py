7393e9ec4997238d6705907cac9b9eb9d8b7b0de
dooa.ansari <dooa.ansari@s2022.tu-chemnitz.de>
2024-02-21 11:54:44 +0100

    module replaced with module#

--- Backend/across/pdf_To_rdf/upload_file_to_blazegraph.py
+++ Backend/across/pdf_To_rdf/upload_file_to_blazegraph.py
@@ -12,8 +12,10 @@ def upload_file_to_blazegraph(directory, file, isFile):
         headers = {'Content-Type': 'application/rdf+xml'}
         for root, dirs, files in os.walk(os.path.abspath(directory)):
          for file in files:
+            with open(os.path.join(root, file), 'rb') as rdf_file:
+              graph.parse(rdf_file, format='xml')
         #   data1 = open(os.path.join(root, file),'r', encoding='utf-8').read()
-          graph.parse(os.path.join(root, file),'r', encoding='utf-8')
+        #   graph.parse(os.path.join(root, file),'r', encoding='utf-8')
         #   print(data1)
         #   datae = parse.urlencode(data1).encode("utf-8")
         #   req = urllib2.Request(url="http://13.51.109.79/blazegraph/namespace/kb/sparql", 
@@ -28,7 +30,7 @@ def upload_file_to_blazegraph(directory, file, isFile):
         #       <a>б</a>"""
         data_xml = graph.serialize(format='xml')
         # print(data_xml)
-        result = requests.post("http://13.51.109.79/blazegraph/namespace/kb/sparql", data=data_xml, headers={'Content-Type': 'application/xml'})
+        result = requests.post("http://13.51.109.79/blazegraph/namespace/kb/sparql", data=data_xml.encode('utf-8'), headers={'Content-Type': 'application/xml'})
         print(result.content)
         
     else:
