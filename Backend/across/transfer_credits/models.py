d501d96229766772c500a2835ed96ed020aaa9d5
Akshay Ghavale <akshay.ghavale@s2022.tu-chemnitz.de>
2024-02-24 15:56:41 +0100

    TransferCreditRequest Model Created and jsx updated

--- Backend/across/transfer_credits/models.py
+++ Backend/across/transfer_credits/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from user import models as userModels
 
-# Create your models here.
+class TransferCredits(models.Model):
+    # Define email as a foreign key
+    email = models.ForeignKey(userModels.UserProfile, to_field='email', on_delete=models.CASCADE)
+
+    # Other Fields
+    status = models.TextField()
+    fromModules = models.JSONField(null=True)
+    toModules = models.JSONField(null=True)
+    # Timestamp fields
+    created_at = models.DateTimeField(null=True, blank=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.email} - Status: {self.status}, Created At: {self.created_at}, Updated At: {self.updated_at}"
